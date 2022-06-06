bcb771563d58cf1d521999f9b0c9471d0b2c9ad4
JhonThe17 <83646868+JhonThe17@users.noreply.github.com>
2022-06-07 01:04:28 +0900

    AFL4

--- ToDo list/TodoList/Views/Home.swift
+++ ToDo list/TodoList/Views/Home.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct Home: View {
     
+    @State var animate : Bool = false
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
@@ -18,8 +20,32 @@ struct Home: View {
                     .padding(.top, 150)
                 
                 Text("List what you want to do for 1 day.. let's be more productive!")
+            }
                     .padding([.leading, .bottom, .trailing], 10.0)
+                    .multilineTextAlignment(.center)
+                    .onAppear(perform: addAnimation)
+                    .scaleEffect(animate ? 1.05 : 1.0)
+                    .foregroundColor(Color.white)
+                    .colorMultiply(animate ? Color.purple : Color.pink)
+                    .shadow(color: animate ? Color.purple.opacity(0.20) : Color.pink.opacity(0.20),
+                            radius: animate ? 10 : 1,
+                            x: 2,
+                            y: 2)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
+            }
+                
+        func addAnimation() {
+            guard !animate else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now()+1.5){
+                withAnimation(
+                    Animation
+                        .easeInOut(duration: 2.5)
+                        .repeatForever()
+                ){
+                    animate.toggle()
+                }
             }
         }
     }
@@ -32,4 +58,4 @@ struct Home_Previews: PreviewProvider {
             }
         }
     }
-}
+
--- ToDo list/TodoList/Views/ToDoList.swift
+++ ToDo list/TodoList/Views/ToDoList.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ToDoList: View {
     
     @EnvironmentObject var todoviewmodel : TodoViewModel
+    @AppStorage("_showOnboarding") var showOnboarding : Bool = true
     
     var body: some View {
         ZStack {
@@ -30,12 +31,80 @@ struct ToDoList: View {
                 .listStyle(SidebarListStyle())
             }
         }
+        .fullScreenCover(isPresented: $showOnboarding, content: {
+            OnboardingView(showOnboarding: $showOnboarding)
+        })
         .navigationTitle("Your Priority 📋")
         .navigationBarItems(
             leading: EditButton(),
             trailing:
                 NavigationLink("Add", destination: AddView())
             )
+    }
+    
+    struct OnboardingView: View {
+        @Binding var showOnboarding: Bool
+        
+        var body: some View{
+            
+            VStack(alignment: .leading, spacing: 10){
+                Text("Welcome to To Do List with this app you can...")
+                    .multilineTextAlignment(.center)
+                    .font(.title)
+                    
+                HStack{
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+                    Spacer()
+                    Text("Add important tasks to the list")
+                        .bold()
+                        .padding()
+                    
+                    
+                }.padding()
+                
+                
+                HStack{
+                    Image(systemName: "checklist")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+                    Spacer()
+                    
+                    Text("Rearrange your tasks and check finished tasks")
+                        .bold()
+                        .padding()
+                }.padding()
+                
+                HStack{
+                    Image(systemName: "delete.left")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.blue)
+                    Spacer()
+                    
+                    Text("Delete finished or unused tasks")
+                        .bold()
+                        .padding()
+                }.padding()
+            }
+            .padding(50)
+            Spacer()
+            
+            Button(action: {
+                showOnboarding.toggle()
+            }, label: {
+                Text("Get Started")
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            })
+            .padding()
+        }
     }
 
 struct ToDoList_Previews: PreviewProvider {
